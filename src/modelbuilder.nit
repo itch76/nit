itch76/nit

commit 1a3536b60b1d02bbfb692b6675b1e302686ff954

modelbuilder: avoid multiple load of the same module
Signed-off-by: Alexandre Terrasa <alexandre@moz-code.org>

src/modelbuilder.nit
@@ -359,14 +359,26 @@ class ModelBuilder
 		return path
 	end
 
+	# loaded module by absolute path
+	private var loaded_nmodules = new HashMap[String, AModule]
+
 	# Try to load a module using a path.
 	# Display an error if there is a problem (IO / lexer / parser) and return null
 	# Note: usually, you do not need this method, use `get_mmodule_by_name` instead.
 	fun load_module(owner: nullable MModule, filename: String): nullable AModule
 	do
+		if filename.file_extension != "nit" then
+			self.toolcontext.error(null, "Error: file {filename} is not a valid nit module.")
+			return null
+		end
 		if not filename.file_exists then
 			self.toolcontext.error(null, "Error: file {filename} not found.")
 			return null
+		end
+
+		var module_path = module_absolute_path(filename)
+		if loaded_nmodules.keys.has(module_path) then
+			return loaded_nmodules[module_path]
 		end
 
 		var x = if owner != null then owner.to_s else "."
@@ -405,6 +417,7 @@ class ModelBuilder
 		nmodule.mmodule = mmodule
 		nmodules.add(nmodule)
 		self.mmodule2nmodule[mmodule] = nmodule
+		self.loaded_nmodules[module_path] = nmodule
 
 		build_module_importation(nmodule)
 

Makefile
@@ -31,7 +31,7 @@ doc/stdlib/index.html: bin/nitdoc
 	@echo '***************************************************************'
 	@echo '* Generate doc for NIT standard library                       *'
 	@echo '***************************************************************'
-	bin/nitdoc lib/*.nit $$(find lib/* -maxdepth 0 -type d ) -d doc/stdlib \
+	bin/nitdoc lib/*.nit $$(find lib/*/*.nit -maxdepth 0 -type f ) -d doc/stdlib \
 		--custom-title "Nit Standard Library" \
 		--custom-menu-items "<li><a href=\"http://nitlanguage.org/\">Nitlanguage.org</a></li>" \
 		--custom-overview-text "<p>Documentation for the standard library of Nit<br/>Version $$(git describe)<br/>Date: $$(git show --format="%cd" | head -1)</p>" \

commit 72116a18cb39c3679250cf95c78bf3fb3bf0f85c

modelbuilder: Remove duplicates from modules list
Fix a mistake introduced by 1a3536b6 (blame me)
Also fix issues #153, #157, #223 and #224

Signed-off-by: Alexandre Terrasa <alexandre@moz-code.org>

src/modelbuilder.nit
@@ -132,11 +132,9 @@ class ModelBuilder
 		var time0 = get_time
 		# Parse and recursively load
 		self.toolcontext.info("*** PARSE ***", 1)
-		var mmodules = new Array[MModule]
 		for a in modules do
 			var nmodule = self.load_module(null, a)
 			if nmodule == null then continue # Skip error
-			mmodules.add(nmodule.mmodule.as(not null))
 		end
 		var time1 = get_time
 		self.toolcontext.info("*** END PARSE: {time1-time0} ***", 2)
@@ -146,6 +144,11 @@ class ModelBuilder
 		if toolcontext.opt_only_parse.value then
 			self.toolcontext.info("*** ONLY PARSE...", 1)
 			exit(0)
+		end
+
+		var mmodules = new Array[MModule]
+		for nmodule in loaded_nmodules.values do
+			mmodules.add(nmodule.mmodule.as(not null))
 		end
 
 		return mmodules
@@ -405,7 +408,11 @@ class ModelBuilder
 		var tree = parser.parse
 		file.close
 		var mod_name = filename.basename(".nit")
-		return load_module_commons(owner, tree, mod_name)
+
+		var nmodule = load_module_commons(owner, tree, mod_name)
+		if nmodule != null then loaded_nmodules[module_path] = nmodule
+
+		return nmodule
 	end
 
 	fun load_rt_module(owner: MModule, nmodule: AModule, mod_name: String): nullable AModule